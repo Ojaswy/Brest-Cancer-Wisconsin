a1fd52df5f10b8ba1e70c602dcb1315b5aae2ca7
Ojaswy Akella <32218681+Ojaswy@users.noreply.github.com>
2018-07-11 14:29:00 +0530

    Add files via upload

---
+++ basic_plots.R
@@ -0,0 +1,55 @@
+#intialising data 
+bc_data <- read.table("datasets/breast-cancer-wisconsin.data.txt", 
+                      header = FALSE, 
+                      sep = ",")
+#column names
+colnames(bc_data) <- c("sample_code_number", 
+                       "clump_thickness", 
+                       "uniformity_of_cell_size", 
+                       "uniformity_of_cell_shape", 
+                       "marginal_adhesion", 
+                       "single_epithelial_cell_size", 
+                       "bare_nuclei", 
+                       "bland_chromatin", 
+                       "normal_nucleoli", 
+                       "mitosis", 
+                       "classes")
+#classifying
+bc_data$classes <- ifelse(bc_data$classes == "2", "benign",
+                          ifelse(bc_data$classes == "4", "malignant", NA))
+
+bc_data[bc_data == "?"] <- NA
+
+# how many NAs are in the data
+length(which(is.na(bc_data)))
+## [1] 16
+# how many samples would we loose, if we removed them?
+nrow(bc_data)
+## [1] 699
+nrow(bc_data[is.na(bc_data), ])
+## [1] 16
+
+
+# impute missing data using MICE
+library(mice)
+
+bc_data[,2:10] <- apply(bc_data[, 2:10], 2, function(x) as.numeric(as.character(x)))
+dataset_impute <- mice(bc_data[, 2:10],  print = FALSE)
+bc_data <- cbind(bc_data[, 11, drop = FALSE], mice::complete(dataset_impute, 1))
+
+bc_data$classes <- as.factor(bc_data$classes)
+
+# how many benign and malignant cases are there?
+summary(bc_data$classes)
+
+library(ggplot2)
+
+ggplot(bc_data, aes(x = classes, fill = classes)) +
+  geom_bar()
+
+library(tidyr)
+
+gather(bc_data, x, y, clump_thickness:mitosis) %>%
+  ggplot(aes(x = y, color = classes, fill = classes)) +
+  geom_density(alpha = 0.3) +
+  facet_wrap( ~ x, scales = "free", ncol = 3)
---
+++ regression.R
@@ -0,0 +1,81 @@
+#intialising data 
+bc_data <- read.table("datasets/breast-cancer-wisconsin.data.txt", 
+                      header = FALSE, 
+                      sep = ",")
+#column names
+colnames(bc_data) <- c("sample_code_number", 
+                       "clump_thickness", 
+                       "uniformity_of_cell_size", 
+                       "uniformity_of_cell_shape", 
+                       "marginal_adhesion", 
+                       "single_epithelial_cell_size", 
+                       "bare_nuclei", 
+                       "bland_chromatin", 
+                       "normal_nucleoli", 
+                       "mitosis", 
+                       "classes")
+#classifying
+bc_data$classes <- ifelse(bc_data$classes == "2", "benign",
+                          ifelse(bc_data$classes == "4", "malignant", NA))
+
+bc_data[bc_data == "?"] <- NA
+
+
+library(mice)
+
+bc_data[,2:10] <- apply(bc_data[, 2:10], 2, function(x) as.numeric(as.character(x)))
+dataset_impute <- mice(bc_data[, 2:10],  print = FALSE)
+bc_data <- cbind(bc_data[, 11, drop = FALSE], mice::complete(dataset_impute, 1))
+
+bc_data$classes <- as.factor(bc_data$classes)
+
+
+set.seed(42)
+index <- createDataPartition(bc_data$classes, p = 0.7, list = FALSE)
+train_data <- bc_data[index, ]
+test_data  <- bc_data[-index, ]
+library(dplyr)
+
+rbind(data.frame(group = "train", train_data),
+      data.frame(group = "test", test_data)) %>%
+  gather(x, y, clump_thickness:mitosis) %>%
+  ggplot(aes(x = y, color = group, fill = group)) +
+  geom_density(alpha = 0.3) +
+  facet_wrap( ~ x, scales = "free", ncol = 3)
+
+set.seed(42)
+model_glm <- caret::train(clump_thickness ~ .,
+                          data = train_data,
+                          method = "glm",
+                          preProcess = c("scale", "center"),
+                          trControl = trainControl(method = "repeatedcv", 
+                                                  number = 10, 
+                                                  repeats = 10, 
+                                                  savePredictions = TRUE, 
+                                                  verboseIter = FALSE))
+model_glm
+
+
+predictions <- predict(model_glm, test_data)
+# model_glm$finalModel$linear.predictors == model_glm$finalModel$fitted.values
+data.frame(residuals = resid(model_glm),
+           predictors = model_glm$finalModel$linear.predictors) %>%
+  ggplot(aes(x = predictors, y = residuals)) +
+    geom_jitter() +
+    geom_smooth(method = "lm")
+	
+	
+	# y == train_data$clump_thickness
+data.frame(residuals = resid(model_glm),
+           y = model_glm$finalModel$y) %>%
+  ggplot(aes(x = y, y = residuals)) +
+    geom_jitter() +
+    geom_smooth(method = "lm")
+
+
+data.frame(actual = test_data$clump_thickness,
+           predicted = predictions) %>%
+  ggplot(aes(x = actual, y = predicted)) +
+    geom_jitter() +
+    geom_smooth(method = "lm")
+
